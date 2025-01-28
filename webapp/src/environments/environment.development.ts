3d93484517a1d5f8aae7103d796fc341d4dfd9be
PiyushYadav-GitHub <157522729+PiyushYadav1414@users.noreply.github.com>
2025-01-27 21:41:33 -0500

    Update environment.development.ts

--- webapp/src/environments/environment.development.ts
+++ webapp/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:3000',  // Remote API server for production
+    baseUrl: 'https://shopeasy-ecoom-website-backend.onrender.com',  // Remote API server for production
   };
   
